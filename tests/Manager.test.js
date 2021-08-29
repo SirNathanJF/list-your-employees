439b56598a046858d325b980e912cb3cd6cf19ee
Nathan Flessner <nathanflessner1@gmail.com>
2021-08-29 16:52:38 -0600

    wrote testing for Manager.js

---
+++ tests/Manager.test.js
@@ -0,0 +1,27 @@
+const Manager = require("../lib/Manager");
+const Employee = require('../lib/Employee');
+
+describe('Manager', () => {
+    describe("Initilization", () => {
+        it("should set an office number with the constructor argument", () => {
+            const testOffice = 48;
+            const newManager = new Manager("Foo", 1, "test@testing.com");
+
+            expect(newManager.officeNumber).toBe(testOffice);
+        });
+
+        it('should return "Manager" when using the getPosition() method', () =>{
+            const testPosition = "Manager";
+            const newManager = new Manager("Foo", 1, "test@testing.com", 48);
+
+            expect(newManager.getPosition()).toBe(testPosition);
+        });
+
+        it("should get office number via getOfficeNumber() method", () => {
+            const testOffice = 48;
+            const newManager = new Manager("Foo", 1, "test@testing.com", testOffice);
+
+            expect(newManager.getOfficeNumber()).toBe(testOffice);
+        });
+    });
+});
